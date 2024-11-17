4d9a07d720378b3106cb007d077d00536e312137
Jusair <jusairjsr123@gmail.com>
2024-11-17 06:35:45 +0530

    updated

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 urlpatterns = [
 
-    path('',views.list,name='list')
+    path('',views.Home,name='Home'),
+    path('list_all/',views.list_all,name='list_all'),
+    path('list_one/<pk>',views.list_one,name='list_one'),
+    path('search/',views.search,name='search'),
 
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
-
+from .models import Product
+from django.contrib.auth.decorators import login_required
 # Create your views here.
-def list(request):
-    return render(request,'background.html')
+def Home(request):
+    products=Product.objects.order_by('id')[:3]
+    return render(request,'Index.html',{'products':products})
+
+# @login_required(login_url='login')
+def list_all(request):
+    all_products=Product.objects.all()
+    context={
+        'all_products':all_products
+    }
+    return render(request,'list_all.html',context)
+
+def list_one(request,pk):
+    product=Product.objects.get(pk=pk)
+    return render(request,'list_one.html',{'product':product})
+
+def search(request):
+    return render(request,'search.html')
